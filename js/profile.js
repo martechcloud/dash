4be39b0bf14f4a49f95b24e54fd17c1435ce4ad3
martechcloud <127006845+martechcloud@users.noreply.github.com>
2024-05-05 17:22:54 +0530

    Add files via upload

--- js/profile.js
+++ js/profile.js
@@ -1,14 +1,37 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", function() {
+
+    // For main profile
     var profileusername = sessionStorage.getItem("profileusername");
     var profilerole = sessionStorage.getItem("profilerole");
 
     var secondaryprofileusername = sessionStorage.getItem("secondaryprofileusername");
     var secondaryprofilerole = sessionStorage.getItem("secondaryprofilerole");
-
+    
     document.getElementById('profileusername').textContent = profileusername; 
     document.getElementById('profilerole').textContent = profilerole;
 
+    // For myprofile page
+
+    var usernamedetails = sessionStorage.getItem("usernamedetails");
+    var roledetails = sessionStorage.getItem("roledetails");
+    var firstNamedetails = sessionStorage.getItem("firstNamedetails");
+    var lastNamedetails = sessionStorage.getItem("lastNamedetails");
+    var emaildetails = sessionStorage.getItem("emaildetails");
+    var phoneNumberdetails = sessionStorage.getItem("phoneNumberdetails");
+    console.log(usernamedetails)
+    try {
+
+    document.getElementById('usernamedetails').value = usernamedetails; 
+    document.getElementById('roledetails').value = roledetails; 
+    document.getElementById('firstNamedetails').value = firstNamedetails; 
+    document.getElementById('lastNamedetails').value = lastNamedetails; 
+    document.getElementById('emaildetails').value = emaildetails; 
+    document.getElementById('phoneNumberdetails').value = phoneNumberdetails; 
+    } catch (error){
+    }
+    
+    // For setting page
     try {
     document.getElementById('primaryprofileusername').textContent = profileusername;
     document.getElementById('primaryprofilerole').textContent = profilerole;
