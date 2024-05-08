6c72f7e13433a233daec2b6de2f7fa918da23ff2
martechcloud <127006845+martechcloud@users.noreply.github.com>
2024-05-08 18:29:07 +0530

    Update encryptionpass.js

--- js/encryptionpass.js
+++ js/encryptionpass.js
@@ -7,12 +7,13 @@ fetch('Encryption Password/Encryption_Password.txt')
     })
     .then(text => {
         // Extracting the password from the text
-        const passwordRegex = /password:\s*(\w+)/i;
+        const passwordRegex = /password:\s*(\w+)/i; // Assuming password format: "password: your_password_here"
         const match = text.match(passwordRegex);
         if (match && match[1]) {
             const password = match[1];
             // Storing the password in session storage
             sessionStorage.setItem('pass', password);
+            console.log(password)
         } else {
             throw new Error('Password not found in the text file');
         }
