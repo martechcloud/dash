a68ddfe7392022188071c676361966cf2e6ce3e7
martechcloud <127006845+martechcloud@users.noreply.github.com>
2024-05-08 18:24:19 +0530

    Create encryptionpass.js

---
+++ js/encryptionpass.js
@@ -0,0 +1,22 @@
+fetch('Encryption Password/Encryption_Password.txt')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(text => {
+        // Extracting the password from the text
+        const passwordRegex = /password:\s*(\w+)/i;
+        const match = text.match(passwordRegex);
+        if (match && match[1]) {
+            const password = match[1];
+            // Storing the password in session storage
+            sessionStorage.setItem('pass', password);
+        } else {
+            throw new Error('Password not found in the text file');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
